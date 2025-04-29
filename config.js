1679bf876020ed654b6c38522659b01737ce0c97
sonamoni119 <donrajput981@gmail.com>
2025-04-29 14:12:00 +0530

    .

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "B.m.b-Xmd~1a8hGTIL#36fNhdZhNJhPbRPSRtpJLtvYGNT8fYl1orXQhPENXkE",
 // add your Session Id 
 PREFIX: process.env.PREFIX || ".",
 // add your prifix for bot
